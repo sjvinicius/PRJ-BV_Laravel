a7e8af28b80c0adb295f7eb819b4b8650a60db5f
Vinicius <sjf.vinicius@gmail.com>
2024-09-11 00:58:09 -0300

    adicionando cart na branch

--- prjapa/resources/views/system/prelayout.blade.php
+++ prjapa/resources/views/system/prelayout.blade.php
@@ -34,6 +34,9 @@
     </div>
     @yield('footer')
     <script src="https://kit.fontawesome.com/e29123cf07.js" loading=“lazy” crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
     <!-- <script src="//cdn.datatables.net/2.0.7/js/dataTables.min.js"></script> -->
     @yield('script')
 </body>
