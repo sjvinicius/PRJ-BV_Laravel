e5253616f69fa0c1b0b62b94c665ac01e2f06b8a
Vinicius <sjf.vinicius@gmail.com>
2024-09-11 00:28:15 -0300

    ajuste nome da tabela produto

--- prjapa/app/Models/Product.php
+++ prjapa/app/Models/Product.php
@@ -9,6 +9,8 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $table = "produto";
+
     protected $fillable = [
         'NOME',
         'VALORMEDIDA',
--- prjapa/app/Models/User.php
+++ prjapa/app/Models/User.php
@@ -11,9 +11,6 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-
-    protected $table = "produto";
-
     /**
      * The attributes that are mass assignable.
      *
